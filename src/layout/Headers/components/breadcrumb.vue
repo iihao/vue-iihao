77b780a118531c824659f4ca05ec6e4b49e6a7f5
暗号 <190897858@qq.com>
2022-08-16 15:27:58 +0800

    update

---
+++ src/layout/Headers/components/breadcrumb.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-breadcrumb separator="/">
+    <el-breadcrumb-item v-for="(item, index) in breadList" :key="index">
+      {{ item.name }}
+    </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+<script lang="ts" setup>
+import { RouteLocationMatched, useRoute } from 'vue-router'
+import { Ref, ref, watch } from 'vue'
+const route = useRoute()
+const breadList: Ref<RouteLocationMatched[]> = ref([])
+const initBreadList = () => {
+  breadList.value = route.matched
+  console.log(route.matched)
+}
+watch(
+  route,
+  () => {
+    initBreadList()
+  },
+  { deep: true, immediate: true }
+)
+</script>
+<style lang="scss" scoped></style>
