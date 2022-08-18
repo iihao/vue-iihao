bd7c4048f0542efa1a2698a2d6a4ffc9801f0174
暗号 <190897858@qq.com>
2022-08-18 11:37:09 +0800

    update

--- src/views/users/index.vue
+++ src/views/users/index.vue
@@ -1,7 +1,34 @@
 <template>
-  <div>users</div>
+  <el-card>
+    <el-row :gutter="20">
+      <el-col :span="8"
+        ><el-input v-model="queryForm.query" placeholder=""></el-input
+      ></el-col>
+      <el-button type="primary">
+        <el-icon style="vertical-align: middle"> <Search /> </el-icon>
+        <span> {{ $t('table.search') }} </span></el-button
+      >
+      <el-button type="primary">{{ $t('table.adduser') }}</el-button>
+    </el-row>
+
+    <el-table :data="tableData" stripe style="width: 100%,padding-top:15px">
+      <el-table-column prop="date" label="Date" width="180" />
+      <el-table-column prop="name" label="Name" width="180" />
+      <el-table-column prop="address" label="Address" />
+    </el-table>
+  </el-card>
 </template>
 
-<script></script>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { getUserList } from '@/api/user'
+const tableData = ref([])
+const queryForm = ref({
+  query: '',
+  pagenum: 1,
+  pagesize: 2
+})
+console.log(getUserList(queryForm.value))
+</script>
 
 <style lang="scss" scoped></style>
